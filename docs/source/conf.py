6afe5656ed43d393b36988dc2fe147754dc420d5
S-andres0694 <sebasandres0694@gmail.com>
2025-06-01 17:59:07 +0100

    Add GitHub Actions workflow for documentation publishing and update Sphinx configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,28 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'LLMShield'
-copyright = '2025, Aditya Dedhia'
-author = 'Aditya Dedhia'
-release = '2/02/2025'
+project = "LLMShield"
+copyright = "2025, Aditya Dedhia"
+author = "Aditya Dedhia"
+release = "2/02/2025"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc', "myst_parser", "sphinx_rtd_theme"]
+extensions = [
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "myst_parser",
+    "sphinx_rtd_theme",
+    "sphinx.ext.githubpages",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
