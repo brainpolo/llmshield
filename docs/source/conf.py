7faf6e968340968d5ca64283da1b2adb9f0a90f3
S-andres0694 <sebasandres0694@gmail.com>
2025-06-01 17:30:12 +0100

    Add Sphinx documentation setup and update project metadata
    - Create Makefile for Sphinx documentation build commands
    - Add Sphinx configuration file and initial documentation structure
    - Update .gitignore to exclude generated documentation files
    - Enhance pyproject.toml with additional dependencies for documentation
    - Create initial reStructuredText files for package documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'LLMShield'
+copyright = '2025, Aditya Dedhia'
+author = 'Aditya Dedhia'
+release = '2/02/2025'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc', "myst_parser", "sphinx_rtd_theme"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/source/llmshield.matchers.dicts.rst
@@ -0,0 +1,10 @@
+llmshield.matchers.dicts package
+================================
+
+Module contents
+---------------
+
+.. automodule:: llmshield.matchers.dicts
+   :members:
+   :show-inheritance:
+   :undoc-members:
